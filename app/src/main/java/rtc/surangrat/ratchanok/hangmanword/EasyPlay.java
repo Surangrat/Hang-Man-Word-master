7807ce2d0319871fcb1058b374200a56d1b1c3dc
Ratchanok <petand_puen789@hotmail.co.th>
2016-12-02 22:49:22 -0800

    eve image

--- app/src/main/java/rtc/surangrat/ratchanok/hangmanword/EasyPlay.java
+++ app/src/main/java/rtc/surangrat/ratchanok/hangmanword/EasyPlay.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -17,18 +18,25 @@ public class EasyPlay extends AppCompatActivity {
     private int scoreAnInt = 0, timeAnInt = 30, wordAnInt = 0, index = 0;
     private MyConstant myConstant;
     private String[] questionStrings, answerStrings;
-
+    private TextView textView1, textView2, textView3, textView4;
+    private TextView[] textViews;
+    private EditText[] editTexts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_easy_play);
 
+        bindWidget();
+
+        textViews = new TextView[]{textView1, textView2, textView3, textView4};
+        editTexts = new EditText[]{editText, editText2, editText3, editText4};
+
         myConstant = new MyConstant();
         questionStrings = myConstant.getQuestionStrings();
         answerStrings = myConstant.getAnswerStrings();
 
-        bindWidget();
+
 
         checkTime();
 
@@ -49,25 +57,15 @@ public class EasyPlay extends AppCompatActivity {
 
         String[] strings = answerStrings[index].split("");
 
-        for (int i=0;i<strings.length;i++) {
+        for (int i = 0; i < strings.length; i++) {
             Log.d("1decV1", "strings(" + i + ")= " + strings[i]);
         }
 
-        switch (indexWord) {
-            case 0:
-                myCheck(editText);
-                break;
-            case 1:
-                break;
-            case 2:
-                break;
-            case 3:
-                break;
-        }   // switch
+        myCheck(editTexts[indexWord],textViews[indexWord]);
 
     }   // checkWord
 
-    private void myCheck(final EditText editText) {
+    private void myCheck(final EditText editText, final TextView textView) {
 
         editText.addTextChangedListener(new TextWatcher() {
             @Override
@@ -81,7 +79,12 @@ public class EasyPlay extends AppCompatActivity {
                 String s = editText.getText().toString();
                 Log.d("1decV1", "s ==> " + s);
 
-            }
+               textView.setText(s);
+                editText.setVisibility(View.INVISIBLE);
+
+
+
+            }   // onTextChange
 
             @Override
             public void afterTextChanged(Editable editable) {
@@ -94,8 +97,8 @@ public class EasyPlay extends AppCompatActivity {
     private void checkTime() {
 
         if (timeAnInt == 0) {
-            Intent intent = new Intent(EasyPlay.this, ShowScore.class);
-            startActivity(intent);
+//            Intent intent = new Intent(EasyPlay.this, ShowScore.class);
+//            startActivity(intent);
         }   // if
 
         Handler handler = new Handler();
@@ -118,6 +121,10 @@ public class EasyPlay extends AppCompatActivity {
         timeTextView = (TextView) findViewById(R.id.textView4);
         scoreTextView = (TextView) findViewById(R.id.textView5);
         questionTextView = (TextView) findViewById(R.id.textView3);
+        textView1 = (TextView) findViewById(R.id.textView6);
+        textView2 = (TextView) findViewById(R.id.textview7);
+        textView3 = (TextView) findViewById(R.id.textview8);
+        textView4 = (TextView) findViewById(R.id.textview9);
     }
 
 }   // Main Class
